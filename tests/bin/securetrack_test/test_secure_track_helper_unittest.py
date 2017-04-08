4aa66f9ed9cc0ef6869754a529760a0c110a2545
Tzachi Gratziani <tzachi.gratziani@tufin.com>
2017-04-08 18:24:56 +0300

    Mock SecureTrack test cases

--- tests/bin/securetrack_test/test_secure_track_helper_unittest.py
+++ tests/bin/securetrack_test/test_secure_track_helper_unittest.py
@@ -134,9 +134,6 @@ class TestDevices(unittest.TestCase):
         self.mock_get_uri.return_value.status_code = 201
         self.mock_get_uri.return_value.headers = {'location': '1'}
         added_offline_device_id = self.helper.add_offline_device("TEST_DEVICE_123", "Cisco", "router")
-        print()
-        print(added_generic_device_id)
-        print()
         self.assertIsInstance(added_offline_device_id, int)
 
         # Invalid device creation
