df668f60e27936a0bcbcabba27d3ffe1aa9d57c4
Tzachi Gratziani <tzachi.gratziani@tufin.com>
2017-04-09 20:52:16 +0300

    Updating core package files

--- setup.py
+++ setup.py
@@ -20,22 +20,23 @@ setup(
     version=get_version(),
     author="Tufin Solution Engineering",
     author_email="support@tufin.com",
-    url='http://pypi.python.org/pypi/pyTOS/',
-    license='LICENSE.txt',
+    url='http://pypi.python.org/pypi/pytos',
+    license="Apache License 2.0",
     description="The Tufin TOS SDK for Python",
     long_description=open('README.txt').read(),
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     # package_dir={package_name: package_name},
     # package_data={'': ['conf/tufin_api.conf.orig']},
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: Apache Software License',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.4'
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: System :: Distributed Computing',
-        'Operating System :: Unix'
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     install_requires=[
         'netaddr>=0.7.14',
@@ -50,6 +51,5 @@ setup(
         'dnspython3',
         'Mako',
         'pyasn1'
-    ],
-    zip_safe=False
+    ]
 )
